adbdb415b012d8a2ad6a4ce7418c97fe5f0dd8a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-07 07:15:39 +0000

    Deploy Rob-Leggett/ui-react-component-kit to github.com/Rob-Leggett/ui-react-component-kit.git:gh-pages

---
+++ types/components/paragraph/Paragraph.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { ReactNode } from 'react';
+export interface ParagraphProps {
+    children: ReactNode;
+}
+declare const Paragraph: React.SFC<ParagraphProps>;
+export default Paragraph;
---
+++ types/components/paragraph/styledParagraph.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { StyledComponentClass } from 'styled-components';
+/** @component */
+export declare const StyledParagraph: StyledComponentClass<React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>, any, React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>>;
