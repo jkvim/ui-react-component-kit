8befdaff2ec51c571c2335cb4cd4ca22cfa0b795
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-09 06:10:19 +0000

    Deploy Rob-Leggett/ui-react-component-kit to github.com/Rob-Leggett/ui-react-component-kit.git:gh-pages

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,12 @@ import './theme/global';
 /**
  * Components
  */
+export { Alert } from './components/alert';
 export { Label } from './components/label';
+export { Hint } from './components/hint';
+export { Error } from './components/error';
+export { Heading } from './components/heading';
+export { Paragraph } from './components/paragraph';
 export { InputText } from './components/input-text';
 export { InputRadioGroup } from './components/input-radio-group';
 export { InputCheckboxGroup } from './components/input-checkbox-group';
